25c1acba1fa269f7c2867cc27b8ea388eb9aadf7
Zsombor Fazekas <zsombor.fazekas@prezi.com>
2022-08-08 23:08:46 +0200

    Add internal RAM and basic address layouting

--- src/nes/bus.cpp
+++ src/nes/bus.cpp
@@ -2,11 +2,49 @@
 
 namespace nes {
 
+namespace {
+bool IsInRange(uint16_t beg, uint16_t end, uint16_t val) {
+	return (beg <= val) && (val <= end);
+}
+
+} // namespace
+
 uint8_t Bus::Read(uint16_t addr) {
+	if (IsInRange(0x0000, 0x17FF, addr)) { // internal memory
+		return memory_[addr % 0x0800];
+	}
+	if (IsInRange(0x2000, 0x3FFF, addr)) { // PPU registers
+
+	}
+	if (IsInRange(0x4000, 0x4017, addr)) { // APU and I/O registers
+
+	}
+	if (IsInRange(0x4018, 0x401F, addr)) { // Test mode
+
+	}
+	if (IsInRange(0x4020, 0xFFFF, addr)) { // Cartridge
+	}
+
 	return 0;
 }
 
 void Bus::Write(uint16_t addr, uint8_t val) {
+	if (IsInRange(0x0000, 0x17FF, addr)) { // internal memory
+		memory_[addr % 0x0800] = val;
+	}
+	if (IsInRange(0x2000, 0x3FFF, addr)) { // PPU registers
+
+	}
+	if (IsInRange(0x4000, 0x4017, addr)) { // APU and I/O registers
+
+	}
+	if (IsInRange(0x4018, 0x401F, addr)) { // Test mode
+
+	}
+	if (IsInRange(0x4020, 0xFFFF, addr)) { // Cartridge
+
+	}
+}
 
 }
 
--- src/nes/bus.h
+++ src/nes/bus.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstdint>
+#include <array>
 
 namespace nes {
 
@@ -8,6 +9,8 @@ class Bus {
 public:
 	uint8_t Read(uint16_t addr);
 	void Write(uint16_t addr, uint8_t val);
+private:
+	std::array<uint8_t, 2048> memory_;
 };
 
 } // namespace nes
