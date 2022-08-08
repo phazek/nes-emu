52aaeb93064fff3d5be6b0d93a18c94a69e4e786
Zsombor Fazekas <zsombor.fazekas@prezi.com>
2022-08-09 00:20:33 +0200

    Initial NROM mapper implementation

---
+++ src/nes/mappers/mapper_nrom.cpp
@@ -0,0 +1,57 @@
+#include "mapper_nrom.h"
+
+#include "tfm/tinyformat.h"
+#include "../utils.h"
+
+namespace nes::mapper {
+
+namespace {
+
+const std::string kMapperName = "NROM";
+uint16_t kMapperId = 0;
+
+} // namespace
+
+Mapper_NROM::Mapper_NROM(uint8_t* buffer, size_t bufSize, RomDescriptor desc):
+	MapperBase(buffer, bufSize, desc) {}
+
+const std::string& Mapper_NROM::GetName() {
+	return kMapperName;
+}
+
+uint16_t Mapper_NROM::GetId() {
+	return kMapperId;
+}
+
+uint8_t Mapper_NROM::ReadPrg(uint16_t addr) {
+	if (IsInRange(0x8000, 0xBFFF, addr)) {
+		return buffer_[descriptor_.prgRomStart + (addr - 0x8000)];
+	}
+	if (IsInRange(0xC000, 0xFFFF, addr)) {
+		if (descriptor_.prgRomSize > 0x4000) {
+			return buffer_[descriptor_.prgRomStart + (addr - 0x8000)];
+		} else {
+			return buffer_[descriptor_.prgRomStart + (addr - 0xC000)];
+		}
+	}
+
+	tfm::printf("ERROR: Invalid PRG read address at 0x%04X!", addr);
+	assert(false);
+	return 0;
+}
+
+void Mapper_NROM::WritePrg(uint16_t addr, uint8_t val) {
+	tfm::printf("ERROR: Invalid PRG write address at 0x%04X!", addr);
+	assert(false);
+}
+
+uint8_t Mapper_NROM::ReadChar(uint16_t addr) {
+	return buffer_[descriptor_.chrRomStart];
+}
+
+void Mapper_NROM::WriteChar(uint16_t addr, uint8_t val) {
+	tfm::printf("ERROR: Invalid CHR write address at 0x%04X!", addr);
+	assert(false);
+}
+
+} // namespace nes::mapper
---
+++ src/nes/mappers/mapper_nrom.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "mapperbase.h"
+
+namespace nes::mapper {
+
+class Mapper_NROM: public MapperBase {
+public:
+	Mapper_NROM(uint8_t* buffer, size_t bufSize, RomDescriptor desc);
+	virtual const std::string& GetName() override;
+	virtual uint16_t GetId() override;
+	virtual uint8_t ReadPrg(uint16_t addr) override;
+	virtual void WritePrg(uint16_t addr, uint8_t val) override;
+	virtual uint8_t ReadChar(uint16_t addr) override;
+	virtual void WriteChar(uint16_t addr, uint8_t val) override;
+private:
+};
+
+} // namespace nes::mapper
