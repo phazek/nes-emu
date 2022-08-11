f31f69994788dd9df5a67ffaa59554878ebc648d
Zsombor, Fazekas <fazekas.zsombor@gmail.com>
2022-08-11 15:55:10 +0200

    Attach bus to PPU

--- src/nes/ppu.cpp
+++ src/nes/ppu.cpp
@@ -1,13 +1,19 @@
 #include "ppu.h"
 
+#include "bus.h"
 namespace nes {
 
 uint8_t Ppu2C02::Read(uint16_t addr) {
+	// TODO
 	return 0;
 }
 
 void Ppu2C02::Write(uint16_t addr, uint8_t val) {
+	// TODO
+}
 
+void Ppu2C02::AttachBus(Bus* bus) {
+	bus_ = bus;
 }
 
 } // namespace nes
--- src/nes/ppu.h
+++ src/nes/ppu.h
@@ -4,12 +4,16 @@
 
 namespace nes {
 
+class Bus;
+
 class Ppu2C02 {
 public:
 	uint8_t Read(uint16_t addr);
 	void Write(uint16_t addr, uint8_t val);
 
+	void AttachBus(Bus* bus);
 private:
+	Bus* bus_ = nullptr;
 };
 
 } // namespace nes
