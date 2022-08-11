0993a1dd9dcaa59ae0b7701510501e0eb42f026f
Zsombor Fazekas <zsombor.fazekas@prezi.com>
2022-08-11 12:01:57 +0200

    Main function using NesApp

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,26 @@
+#include "olc/olcPixelGameEngine.h"
+#include "tfm/tinyformat.h"
+
+#include "nesapp.h"
+#include "nes/cartridge.h"
+
 int main(int argc, char** argv) {
+	NesApp app;
+	std::string path;
+	if (argc > 1) {
+		path = argv[1];
+	}
+
+	Cartridge cart;
+	if (!cart.LoadFile(path)) {
+		tfm::printf("ERROR: Failed to load ROM from path: %s\n", path);
+		return 1;
+	}
+
+	if (app.Construct(640, 480, 2, 2)) {
+		app.InsertCartridge(&cart);
+		app.Start();
+	}
+
     return 0;
 }
