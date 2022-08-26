f600f3ee9c7519c859fdae0af535a22b162afb60
Zsombor, Fazekas <fazekas.zsombor@gmail.com>
2022-08-26 22:13:41 +0200

    Introduce nameTable and attributeTable

--- src/nes/ppu.h
+++ src/nes/ppu.h
@@ -29,6 +29,10 @@ private:
 		std::array<Palette, 4> backgroundPalettes;
 		std::array<Palette, 4> spritePalettes;
 	} framePalette_;
+
+	std::array<uint8_t, 0x20 * 0x1E> nameTable_;
+	std::array<uint8_t, 0x08 * 0x08> attributeTable_;
+
 	uint8_t status_ = 0;
 
 	uint32_t dotIdx_ = 0;
